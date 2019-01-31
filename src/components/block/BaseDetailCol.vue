1a0ad41b02571a8b37f9c06fe7f4d349bbb7285c
zhangguohua <461974918@qq.com>
2019-01-31 13:35:59 +0800

    'detail_col'

---
+++ src/components/block/BaseDetailCol.vue
@@ -0,0 +1,41 @@
+<!--
+* @Author: Zhang Guohua
+* @Date:   2019-01-31 10:50:13
+* @Last Modified by:   zgh
+* @Last Modified time: 2019-01-31 10:50:20
+* @Description: create by zgh
+* @GitHub: Savour Humor
+-->
+<template>
+  <el-col :span="span || 6" class="item-wrapper">
+    <div class="label"  :style="{background: color || 'transparent'}">{{ label }}:</div>
+    <div class="value"  :style="{background: color || 'transparent'}">{{ value }}</div>
+  </el-col>
+</template>
+
+<script>
+// 一个信息的展示组件，可以用在详情页面。可以用在任务结果中,显示任务的类型,数量。
+// 便于统一样式的展示和管理。
+
+export default {
+  props: ['label', 'value', 'color', 'span'],
+  name: 'BaseDetailCol',
+};
+
+</script>
+
+<style lang="scss" scoped>
+.label, .value {
+  display: table-cell;
+}
+.label {
+  word-break: keep-all;
+}
+.value {
+  word-break: break-all;
+  padding-left: 8px;
+}
+.item-wrapper {
+  margin-bottom: 20px;
+}
+</style>
