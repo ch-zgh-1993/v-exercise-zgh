05a2055bc3a3e555393518a94cb232ab2fd03691
zhangguohua <zhangguohua@shuidihuzhu.com>
2020-06-04 19:11:17 +0800

    feat: custom direction

---
+++ src/views/customDirection/index.vue
@@ -0,0 +1,13 @@
+<!--
+* @Author: Zhang Guohua
+* @Date:   2020-06-04 11:52:16
+* @Last Modified by:   zgh
+* @Last Modified time: 2020-06-04 11:53:25
+* @Description: create by zgh
+* @GitHub: Savour Humor
+-->
+<template>
+	<div>
+		
+	</div>
+</template>
